8e4ac4331d0b463833135e53ecb916912e83a250
Dapaa1105 <dafanr2307@gmail.com>
2025-09-23 11:49:10 +0700

    Ketiga

--- Pertemuan 5/Kolaborasi/Kolaborasi.java
+++ Pertemuan 5/Kolaborasi/Kolaborasi.java
@@ -1,6 +1,6 @@
 public class Kolaborasi {
     public static void main(String[] args) {
         System.out.println("Ibennn");
-        System.out.println("Ibennnn");
+        System.out.println("Daapaa");
     }
 }
