355e88577c1ad121db91e05da32d00b39ea69ba9
Ibnii <ibniandarta9999@gmail.com>
2025-10-23 19:21:43 +0700

    Modifikasi Percobaan 1

--- Pertemuan 9 (Jobsheet7)/SiakadFor13.java
+++ Pertemuan 9 (Jobsheet7)/SiakadFor13.java
@@ -4,8 +4,9 @@ public class SiakadFor13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah =100;
+        int lulus = 0, tidaklulus = 0;
         for (int i = 0; i < 10; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-"+i+": ");
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+": ");
             nilai = sc.nextDouble();
             if(nilai > tertinggi){
                 tertinggi = nilai;
@@ -13,9 +14,14 @@ public class SiakadFor13 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            //perhitungan lulus dan tidak
+            if(nilai>=60) ++lulus; else ++tidaklulus;
         }
         System.out.println("Nilai tertinggi\t: "+tertinggi);
         System.out.println("Nilai terendah\t: "+terendah);
+        System.out.println("Jumlah mahasiswa lulus\t\t: " + lulus );
+        System.out.println("Jumlah mahasiswa tidak lulus\t: "+tidaklulus);
     }
     
 }
