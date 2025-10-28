0816a5072ad5452d852ca3a6a72f92061c94143e
Ibnii <ibniandarta9999@gmail.com>
2025-10-28 18:15:50 +0700

    Percobaan 2

---
+++ Pertemuan 10 (Jobsheet8)/Square13.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Square13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai n\t: ");
+        int n = sc.nextInt();
+
+        for (int iOuter = 0; iOuter < n; iOuter++) {
+            for (int i = 1; i <= n; i++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
