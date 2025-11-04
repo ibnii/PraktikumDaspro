2160562497989683febd3f34d5cf002e67dab56a
Ibnii <ibniandarta9999@gmail.com>
2025-11-04 11:20:55 +0700

    Modifikasi Percobaan 3

--- Pertemuan 11 (Jobsheet9)/ArrayRataNilai13.java
+++ Pertemuan 11 (Jobsheet9)/ArrayRataNilai13.java
@@ -3,19 +3,24 @@ import java.util.Scanner;
 public class ArrayRataNilai13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int nilaiMhs[]= new int[10];
         double total = 0, rata2;
+        int siswaLulus = 0;
+        System.out.print("Masukkan jumlah siswa : ");
+        int jmlSiswa = sc.nextInt();
+        int nilaiMhs[] = new int[jmlSiswa];
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
+            siswaLulus = (nilaiMhs[i] > 70) ? ++siswaLulus : siswaLulus;
         }
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
         }
 
-        rata2 = total/nilaiMhs.length;
+        rata2 = total / nilaiMhs.length;
         System.out.println("Rata rata nilai = " + rata2);
+        System.out.println("Jumlah Mahasiswa lulus = " + siswaLulus);
     }
 }
