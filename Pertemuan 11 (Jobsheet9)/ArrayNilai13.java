a0aae20065807122e8e915b9f122edea073e54c5
Ibnii <ibniandarta9999@gmail.com>
2025-11-04 11:14:25 +0700

    Modifikasi Percobaan 2

--- Pertemuan 11 (Jobsheet9)/ArrayNilai13.java
+++ Pertemuan 11 (Jobsheet9)/ArrayNilai13.java
@@ -10,7 +10,11 @@ public class ArrayNilai13 {
         }
 
         for (int i = 0; i < nilaiAkhir.length; i++) {
-            System.out.println("Nilai akhir ke-"+ i +"adalah "+nilaiAkhir[i]);
+            if (nilaiAkhir[i]>70) {
+                System.out.println("Mahasiswa ke-"+ i +" lulus!");
+            }else{
+                System.out.println("Mahasiswa ke-"+ i +" tidak lulus!");
+            }
         }
     }
 }
