f4640f209c1a9969da49382af411ed38ed348656
Ibnii <ibniandarta9999@gmail.com>
2025-11-11 09:18:02 +0700

    Percobaan 1

---
+++ Pertemuan 12 (Jobsheet10)/Bioskop13.java
@@ -0,0 +1,20 @@
+public class Bioskop13 {
+    public static void main(String[] args) {
+        String[][] penonton = new String[4][2];
+
+        penonton[0][0] = "Amin";
+        penonton[0][1] = "Budi";
+        penonton[1][0] = "Cici";
+        penonton[1][1] = "Dedi";
+        penonton[2][0] = "Eka";
+        penonton[2][1] = "Fani";
+        penonton[3][0] = "Gina";
+        penonton[3][1] = "Hana";
+
+        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+
+    }
+}
