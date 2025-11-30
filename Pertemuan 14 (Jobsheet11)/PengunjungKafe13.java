be596c52920701740ee98cf47b1e0921c4451057
Ibnii <ibniandarta9999@gmail.com>
2025-11-30 15:07:42 +0700

    Modifikasi Percobaan 4

--- Pertemuan 14 (Jobsheet11)/PengunjungKafe13.java
+++ Pertemuan 14 (Jobsheet11)/PengunjungKafe13.java
@@ -8,8 +8,8 @@ public class PengunjungKafe13 {
 
     static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String string : namaPengunjung) {
+            System.out.println("- " + string);
         }
     }
 
